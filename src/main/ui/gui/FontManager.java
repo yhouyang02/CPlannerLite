37ba237c4c5c2d5d81ab8fdf035b2de047a51904
yuhengo <yuhengo@students.cs.ubc.ca>
2021-08-12 23:43:49 +0800

    Renamed font constants

--- src/main/ui/gui/FontManager.java
+++ src/main/ui/gui/FontManager.java
@@ -6,30 +6,30 @@ import java.awt.*;
 // Represents all fonts used in this application
 public class FontManager extends UIManager {
 
-    public static final Font UIFont = new Font("Open Sans", Font.PLAIN, 22);
-    public static final Font messageFont = new Font("Verdana", Font.PLAIN, 22);
-    public static final Font contentFont = new Font("Verdana", Font.PLAIN, 24);
+    public static final Font UI_FONT = new Font("Open Sans", Font.PLAIN, 22);
+    public static final Font MESSAGE_FONT = new Font("Verdana", Font.PLAIN, 22);
+    public static final Font CONTENT_FONT = new Font("Verdana", Font.PLAIN, 24);
 
     // EFFECTS: sets the fonts of this application
     public static void setFonts() {
         // Setting message fonts
-        put("OptionPane.font", messageFont);
-        put("OptionPane.messageFont", messageFont);
-        put("Label.font", messageFont);
-        put("CheckBox.font", messageFont);
-        put("TextField.font", messageFont);
-        put("RadioButton.font", messageFont);
-        put("ComboBox.font", messageFont);
+        put("OptionPane.font", MESSAGE_FONT);
+        put("OptionPane.messageFont", MESSAGE_FONT);
+        put("Label.font", MESSAGE_FONT);
+        put("CheckBox.font", MESSAGE_FONT);
+        put("TextField.font", MESSAGE_FONT);
+        put("RadioButton.font", MESSAGE_FONT);
+        put("ComboBox.font", MESSAGE_FONT);
 
         // Setting UI fonts
-        put("OptionPane.buttonFont", UIFont);
-        put("MenuBar.font", UIFont);
-        put("Menu.font", UIFont);
-        put("MenuItem.font", UIFont);
-        put("MenuItem.acceleratorFont", UIFont);
+        put("OptionPane.buttonFont", UI_FONT);
+        put("MenuBar.font", UI_FONT);
+        put("Menu.font", UI_FONT);
+        put("MenuItem.font", UI_FONT);
+        put("MenuItem.acceleratorFont", UI_FONT);
 
         // Setting content fonts
-        put("TextArea.font", contentFont);
+        put("TextArea.font", CONTENT_FONT);
     }
-    
+
 }
